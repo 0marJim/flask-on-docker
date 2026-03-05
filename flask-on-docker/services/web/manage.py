4d3cb690e5a8692cea3d37000b42df6699c3b4c0
0marJim <omar.jimenez.us@gmail.com>
2026-03-04 22:37:40 -0800

    building docker

--- flask-on-docker/services/web/manage.py
+++ flask-on-docker/services/web/manage.py
@@ -6,5 +6,12 @@ from project import app
 cli = FlaskGroup(app)
 
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
 if __name__ == "__main__":
     cli()
